e12ee5ab952a2d732a45aea448a9d447994e909f
root <morpheusou@hotmail.com>
2015-08-24 09:09:30 -0400

    fixed bug

--- echo/echoclient_1.cpp
+++ echo/echoclient_1.cpp
@@ -1,4 +1,6 @@
 #include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
 #include <arpa/inet.h>
 #include <cstring>
 #include <iostream>
@@ -11,10 +13,19 @@ int main()
 	bzero(&addr, sizeof(addr));
 	addr.sin_family = AF_INET; addr.sin_port = htons(12345);
 	inet_pton(AF_INET, "10.80.45.149", &addr.sin_addr);
-        int client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	
-	int conn_fd = connect(client_fd, (struct sockaddr *)&addr, sizeof (addr));
-	cout << "connection from" << inet_ntoa(addr.sin_addr) << "Port:" << ntohs(addr.sin_port) << endl;        
-        cout << "connection is finished." << endl;
+	int client_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	int conn_status = connect(client_fd, (struct sockaddr *)&addr, sizeof (addr));
+
+	if (conn_status)
+	{
+		cout << "connection failed, reason is : " << strerror(errno) << endl;
+	}
+	else
+	{
+		cout << "connection from" << inet_ntoa(addr.sin_addr) << "Port:" << ntohs(addr.sin_port) << endl;        
+		cout << "connection is finished." << endl;
+		close(client_fd);
+	}
 	return 0;
 }
--- echo/echoserv_1.cpp
+++ echo/echoserv_1.cpp
@@ -5,12 +5,14 @@
 #include <arpa/inet.h>
 #include <cstring>
 #include <iostream>
+#include <errno.h>
 using namespace std;
 
 #define max_connections 10000
 
 void server_handler(int servfd)
 {
+	cout << "server function activation" << endl;
 }
 
 int main()
@@ -21,19 +23,27 @@ int main()
 	socklen_t sock_len = 0;
 	bzero(&addr, sizeof(addr));
 	addr.sin_family = AF_INET;
-	addr.sin_port = htons(6977);
+	addr.sin_port = htons(12345);
 	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 	int fd_server = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	bind(fd_server, (struct sockaddr*)&addr, sizeof(addr));
 	listen(fd_server, max_connections);
 	cout << "Server started\nlistening..." << endl;
-	for (;;) {
-		int fd_client = accept(fd_server, (struct sockaddr*)&client_addr, &sock_len);
-		cout << "connection from " << inet_ntoa(client_addr.sin_addr) 
-		     << "port : " << ntohs(client_addr.sin_port) << endl; 
-		server_handler(fd_client);
 
-		close(fd_client);
+	int client_fd[max_connections] = {0};
+	for (int i = 0; i < max_connections; ++i) {
+		client_fd[i] = accept(fd_server, (struct sockaddr*)&client_addr, &sock_len);
+
+		if (-1 == client_fd[i]) {
+			cout << "connection failed, reason is : " << strerror(errno) << endl;
+		}else{	
+
+			cout << "connection from " << inet_ntoa(client_addr.sin_addr) 
+				<< "port : " << ntohs(client_addr.sin_port) << endl; 
+			server_handler(client_fd);
+
+			close(fd_client);
+		}
 	}
 	return 0;
 }
